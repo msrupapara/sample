5e663ad997da2bf1e0d4a15ffe5849b4a49613a1
Sachin Gevariya <s.gevariya@gmail.com>
2017-09-26 11:48:46 +0530

    changes in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :microsoft_live,'d4bd00d9-4f1a-49cf-a6f5-57ccd7e088a6', 'E229F6A6EBA4CF61ED2185BB48E6716C77CEA713'
+  config.omniauth :microsoft_live,'d4bd00d9-4f1a-49cf-a6f5-57ccd7e088a6', 'k7GkjLbncEZnqUDP93EZ8cp'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
